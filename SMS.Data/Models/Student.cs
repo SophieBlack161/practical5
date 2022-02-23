45070bd9af6ccfff6f578e0d191c95442b048416
Sophie Black <Black-s31@ulster.ac.uk>
2022-02-23 14:30:14 +0000

    Question-14 completed

--- SMS.Data/Models/Student.cs
+++ SMS.Data/Models/Student.cs
@@ -1,5 +1,5 @@
 using System;
-using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;//needed to allow required annotations
 
 namespace SMS.Data.Models
 {
@@ -7,16 +7,24 @@ namespace SMS.Data.Models
     {
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
-
+        
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
+
+        [Required]
         public string Course { get; set; }
-
+        
+        [Range (13,90)]
         public int Age { get; set; } 
-
+        
+        [Range (0,100)]
         public double Grade { get; set; }
-
+        
+        [Url]
         public string PhotoUrl { get; set; }     
 
         // Read-Only property - not stored in database
--- SMS.Data/Services/StudentServiceSeeder.cs
+++ SMS.Data/Services/StudentServiceSeeder.cs
@@ -18,7 +18,7 @@ namespace SMS.Data.Services
             svc.AddStudent("Bart Simpson","Maths", "bart@mail.com", 14, 61, "https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png" );
             svc.AddStudent("Lisa Simpson","Poetry", "lisa@mail.com", 13, 80, "https://upload.wikimedia.org/wikipedia/en/e/ec/Lisa_Simpson.png" );
             svc.AddStudent("Mr Burns","Management", "burns@mail.com", 81, 63, "https://static.wikia.nocookie.net/simpsons/images/a/a7/Montgomery_Burns.png" );
-           
+            svc.AddStudent("Moe","Business","moe@mail.com",41,73,"https://compote.slate.com/images/db5e5e2e-2b98-4b97-af5e-83c6c9c3ffee.jpg?width=960");
         }
     }
 }
